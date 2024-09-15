4ec0ce48b59ba24b9f223e847eef8e65a9293b2a
Om Patel <omupatel7113@gmail.com>
2024-09-15 12:24:31 +0530

    Add Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Analytics />
     <div className="bg-red-400 fixed bottom-0 z-50 w-full p-2 font-bold text-lg tracking-wide font-mono text-center text-zinc-800">
       <p>Development Under Process..!! Share your feedback.</p>
     </div>
