3598b3bd59a0a51a162b7f111769786b797f0c77
Om Patel <omipatel7113@gmail.com>
2024-10-01 18:24:08 +0530

    Add installation guide to Dashboard

--- src/pagesComponents/PageShowcase/DashboardPageShowcase.jsx
+++ src/pagesComponents/PageShowcase/DashboardPageShowcase.jsx
@@ -197,6 +197,56 @@ const DashboardPageShowcase = () => {
         Dashboard Page Components
       </h1>
 
+      {/* Installation Guide  */}
+      <h1 className="text-2xl my-2 font-medium tracking-wide"># Installation</h1>
+      <div className="flex justify-between p-2 mb-4 bg-gray-800 rounded-lg border border-gray-700 shadow-lg">
+        <SyntaxHighlighter
+          language="jsx"
+          style={atomDark}
+          className="rounded-md w-full"
+        >
+          npm install framer-motion recharts lucide-react
+        </SyntaxHighlighter>
+        <button
+          onClick={() =>
+            copyToClipboard("npm install framer-motion recharts lucide-react")
+          }
+          className="text-white p-1 sm:p-4"
+        >
+          {copySuccess ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-green-400"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+
       {Object.keys(componentCode).map((componentName) => (
         <div key={componentName} className="mb-8">
           <h2 className="text-2xl sm:text-3xl mb-4">
